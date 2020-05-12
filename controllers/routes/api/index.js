f6737810e3ba74269e6ec184e1a5b1a77754058a
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-05-12 15:33:31 -0400

    added navbar.js code

--- controllers/routes/api/index.js
+++ controllers/routes/api/index.js
@@ -1,16 +1,7 @@
 const router = require("express").Router();
-const booksControllers = require("../../booksControllers");
+const bookRoutes = require("./books");
 
-// matches with /api/books
-router.route("/")
-.get(booksControllers.finalAll)
-.post(booksControllers.create);
-
-// matches with /api/books/:id
-router
-.route("/:id")
-.get(booksControllers.findById)
-.put(booksControllers.update)
-.delete(booksControllers.remove);
+// Book routes
+router.use("/books", bookRoutes);
 
 module.exports = router;
