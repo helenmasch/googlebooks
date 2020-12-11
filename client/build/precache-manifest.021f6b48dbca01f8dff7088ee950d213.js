d5969a1e99822e5a43f7afc3d2e64873c057a0cf
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-12-10 22:04:08 -0500

    comfirming merge

---
+++ client/build/precache-manifest.021f6b48dbca01f8dff7088ee950d213.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c34346acaea811a304232a109f7937",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef5725b7ead89af5feff",
+    "url": "/static/css/main.0ca39b43.chunk.css"
+  },
+  {
+    "revision": "550ec25c92f49b8caa9a",
+    "url": "/static/js/2.21580ed5.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.21580ed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5725b7ead89af5feff",
+    "url": "/static/js/main.c8caa81f.chunk.js"
+  },
+  {
+    "revision": "934616984ed328925a35",
+    "url": "/static/js/runtime-main.38648221.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
